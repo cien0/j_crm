9986b6305a219bed7947a905e4360cde4851f177
cien0 <marekolszewski@marekolszewski.pl>
2018-05-14 22:24:55 +0000

    Kolejna wysyłka

--- app/models/kontrahenci.rb
+++ app/models/kontrahenci.rb
@@ -1,3 +1,12 @@
 class Kontrahenci < ActiveRecord::Base
     has_one :opiekun
+
+
+def text_for_payment_status
+      opiekun_hash = {}
+      Opiekun.select(:id,:login).each do |c|
+      opiekun_hash[c.id]= c.login
+      end
+     @@opiekun_status_data[opiekun_hash]
+end
 end

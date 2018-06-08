d67e9fc9978f2927f20c77a3cbc515e9acf6c1c9
cien0 <marekolszewski@marekolszewski.pl>
2018-06-08 12:52:04 +0000

    Kolejna wysyłka

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -18,18 +18,75 @@ class StatusController < ApplicationController
   end
 
   # GET /status/1/edit
-  def edit
-    @kontrahenci = Kontrahenci.all
-    @obroty = Obroty.all
-    @kontrahenci.each do |kontrahent|
-      ktory=kontrahent.id
-      m1=1.month.ago.month
-      y1=1.month.ago.year
-      @saldo1= Obroty.where({ kontrahenci_id:  ktory, rok: y1, miesiac: m1 }).take
-      pry 
-     
+def edit
+  @kontrahenci = Kontrahenci.all
+  @obroty = Obroty.all
+  @kontrahenci.each do |kontrahent|
+    ktory=kontrahent.id
+    m1=1.month.ago.month
+    y1=1.month.ago.year
+    m2=2.month.ago.month
+    y2=2.month.ago.year
+    m3=3.month.ago.month
+    y3=3.month.ago.year
+    
+    @saldo1= Obroty.where({ kontrahenci_id:  ktory, rok: y1, miesiac: m1 }).take
+    @saldo2= Obroty.where({ kontrahenci_id:  ktory, rok: y2, miesiac: m2 }).take
+    @saldo3= Obroty.where({ kontrahenci_id:  ktory, rok: y3, miesiac: m3 }).take
+      if !@saldo1.nil?  then
+        logger.debug  "Saldo: #{@saldo1['kwota'].to_f}" 
+        if @saldo1['kwota']==0 then
+          Kontrahenci.transaction do
+            k = Kontrahenci.lock.find(kontrahent.id)
+            k.status_text_id = 3
+            k.save!
+          end
+        end
+        if @saldo1.nil?  then
+        Kontrahenci.transaction do
+            k = Kontrahenci.lock.find(kontrahent.id)
+            k.status_text_id = 3
+            k.save!
+          end
+        end
+        if !@saldo1.nil? && !@saldo2.nil? && !@saldo2.nil?  then
+        @suma = @saldo1['kwota'].to_f + @saldo2['kwota'].to_f + @saldo3['kwota'].to_f
+        logger.debug  "Saldo suma: #{@suma}" 
+        if @suma==0 then
+          Kontrahenci.transaction do
+            k = Kontrahenci.lock.find(kontrahent.id)
+            k.status_text_id = 4
+            k.save!
+          end
+        end
+        end
+        if @saldo1.nil?
+          @suma1=0
+        else 
+          @suma1=@saldo1['kwota'].to_f
+        end
+        if @saldo2.nil?
+          @suma2=0
+        else
+          @suma2 = @saldo2['kwota'].to_f
+        end
+        if @saldo2.nil?
+          @suma3=0
+        else
+          @suma3= @saldo3['kwota'].to_f
+        end
+        @suma= @suma1 + @suma2 + @suma3
+        if @suma==0  
+        Kontrahenci.transaction do
+            k = Kontrahenci.lock.find(kontrahent.id)
+            k.status_text_id = 4
+            k.save!
+          end
+        end
+      end
     end
-  end
+  
+end
 
   # POST /status
   # POST /status.json
